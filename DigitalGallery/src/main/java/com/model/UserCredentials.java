0dbf83ce3f16ae9217bf239153c83c4a40af9140
Heera Babu <heerababu80@gmail.com>
2017-05-08 11:40:38 +0530

    Update UserCredentials.java
    just for clear understanding of code

--- DigitalGallery/src/main/java/com/model/UserCredentials.java
+++ DigitalGallery/src/main/java/com/model/UserCredentials.java
@@ -20,30 +20,6 @@ private boolean enabled;
 private String username;
 
 
-public String getPassword() {
-	return password;
-}
-public void setPassword(String password) {
-	this.password = password;
-}
-public String getRole() {
-	return role;
-}
-public void setRole(String role) {
-	this.role = role;
-}
-public boolean isEnabled() {
-	return enabled;
-}
-public void setEnabled(boolean enabled) {
-	this.enabled = enabled;
-}
-public String getUsername() {
-	return username;
-}
-public void setUsername(String username) {
-	this.username = username;
-}
-
+//generate setter and getter
 	
 }
